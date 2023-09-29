6391aa33733a7e1b6d4fbd20ec2f77e102d5a443
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-09-28 22:52:58 -0300

    feat: wip

--- README.md
+++
@@ -1,1 +0,0 @@
-<h1>Livewire world</h1>
--- resources/views/livewire/counter.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <h1 style="color: #000">{{ $count }}</h1>
- 
-    <button wire:click="increment" style="color: #000">+</button>
- 
-    <button wire:click="decrement" style="color: #000">-</button>
-</div>
