149fe15825f85eff7cc334c269c003f2ecb411cd
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-10-06 16:31:27 -0300

    feat: eip

--- to-do-list/app/Livewire/TodoList.php
+++ to-do-list/app/Livewire/TodoList.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class TodoList extends Component
 {
+    public $name;  //name
+
+    public function create()
+    {
+        dd($this->name); // name
+    }
     public function render()
     {
         return view('livewire.todo-list');
